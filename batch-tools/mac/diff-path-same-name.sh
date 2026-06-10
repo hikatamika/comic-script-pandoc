ed4d7847ec985b3486ddddfd0721ed9655d860cc
hikatamika <36945134+hikatamika@users.noreply.github.com>
2026-06-10 15:01:01 -0700

    Better comments

--- batch-tools/mac/diff-path-same-name.sh
+++ batch-tools/mac/diff-path-same-name.sh
@@ -2,7 +2,7 @@
 
 # Paste this file into an Automator Application
 
-# Destination folder
+# Destination folder. Change it if you want your stuff to go somewhere else.
 DEST="$HOME/DestinationFolder"
 
 # Gets created if it doesn't exist
@@ -15,6 +15,11 @@ for file in "$@"; do
     filename=$(basename "$file")
     basename_no_ext="${filename%.*}"
 
+    # Change docx to your starting filetype on the -f line.
+    # I highly recommend putting the lua writer next to your automator app.
+    # Use the absolute path for your lua writer if it's not next to your automator app.
+    # Either change your lua writer's name to writer.lua, or, put the name of the writer.lua you want on the -t line.
+    # The output on the -o line is done for you.
     pandoc -f docx "$file" \
            -t writer.lua \
            -o "$DEST/${basename_no_ext}.tsv"
--- batch-tools/mac/same-path-same-name.sh
+++ batch-tools/mac/same-path-same-name.sh
@@ -8,9 +8,11 @@ for file in "$@"; do
     # Get filename without extension
     base="${file%.*}"
 
-    # Change docx to your starting file type. 
-    # Use the absolute path for your lua writer if it's not next to your automator app
-    # The output is done for you.
+    # Change docx to your starting filetype on the -f line.
+    # I highly recommend putting the lua writer next to your automator app.
+    # Use the absolute path for your lua writer if it's not next to your automator app.
+    # Either change your lua writer's name to writer.lua, or, put the name of the writer.lua you want on the -t line.
+    # The output on the -o line is done for you.
     pandoc -f docx "$file" \
            -t writer.lua \
            -o "${base}.tsv"
