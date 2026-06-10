58a5ee7602cd6096365e261ee50312adcc9c14e5
hikatamika <36945134+hikatamika@users.noreply.github.com>
2026-06-10 15:01:13 -0700

    Better quiet

--- batch-tools/mac/same-path-same-name.quiet.sh
+++ batch-tools/mac/same-path-same-name.quiet.sh
@@ -1,12 +1,6 @@
 for file in "$@"; do
     echo "Converting \"$file\"..."
-
-    # Get filename without extension
     base="${file%.*}"
-
-    # Change docx to your starting file type. 
-    # Use the absolute path for your lua writer if it's not next to your automator app
-    # The output is done for you.
     pandoc -f docx "$file" \
            -t writer.lua \
            -o "${base}.tsv"
